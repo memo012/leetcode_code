692c6469a07c095328fdd0b825b60e4063a6e866
memo012 <1158821459@qq.com>
2020-07-19 17:47:19 +0800

    7.19

---
+++ src/com/adminsys/猿辅导/k个一组翻转链表/Solution.java
@@ -0,0 +1,54 @@
+package com.adminsys.猿辅导.k个一组翻转链表;
+
+/**
+ * @author Qiang
+ * @version 1.0
+ * @description TODO
+ * @date 2020/7/19 下午5:07
+ **/
+
+import java.util.*;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode root = new ListNode(-1);
+        root.next = head;
+        ListNode pre = root;
+        ListNode end = root;
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) end = end.next;
+            if (end == null) break;
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return root.next;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
+        }
+        return pre;
+    }
+
+}
